51d56cb4edd21e5c56cfb33c51e46bcf23249013
Emre Yilmaz <mail@emreyilmaz.me>
2019-01-14 11:43:28 +0300

    Add a simple sync class to post to the STEEM blockchain

---
+++ instasteem/sync.py
@@ -0,0 +1,30 @@
+import json
+
+from lightsteem.client import Client
+from lightsteem.datastructures import Operation
+from slugify import slugify
+
+
+class Sync:
+
+    def __init__(self, client=None, keys=None):
+        self.client = client or Client(keys=keys)
+
+    def post(self, username, title, body, tags):
+        """Sync the parsed post into the STEEM blockchain.
+
+        :param username: Author on STEEM blockchain
+        :param title: Title on the STEEM post
+        :param body: Body of the post
+        :param tags: A list of tags
+        """
+        post = Operation('comment', {
+            "parent_author": None,
+            "parent_permlink": tags[0],
+            "author": username,
+            "permlink": slugify(title),
+            "title": title,
+            "body": body,
+            "json_metadata": json.dumps({"tags": tags})
+        })
+        self.client.broadcast(post)
